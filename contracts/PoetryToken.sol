5dee0893cea8b87c175b0e9615275615c7f676fd
Ekaterina Itsenko <ekaterinaicenko@gmail.com>
2024-04-16 01:55:55 +0200

    Small fixes to the contract file

--- contracts/PoetryToken.sol
+++ contracts/PoetryToken.sol
@@ -4,28 +4,28 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract PoetryToken is ERC721 {
-    uint256 private _tokenIdCounter;
-    mapping(uint256 => string) private _poetryText;
+    uint256 public tokenIdCounter;
+    mapping(uint256 => string) private _poetryMapping;
 
     event PoetryPublished(address indexed author, uint256 indexed tokenId, string poetryText);
 
     constructor() ERC721("PoetryToken", "PT") {}
 
     function publishPoetry(string memory poetryText) external returns (uint256) {
-        _tokenIdCounter++;
-        _mint(msg.sender, _tokenIdCounter);
-        _setPoetryText(_tokenIdCounter, poetryText);
-        return _tokenIdCounter;
+        tokenIdCounter++;
+        _safeMint(msg.sender, tokenIdCounter);
+        _setPoetryText(tokenIdCounter, poetryText);
+        return tokenIdCounter;
     }
 
     function getPoetryText(uint256 tokenId) external view returns (string memory) {
         require(_ownerOf(tokenId) != address(0), "Token doesn't exist");
-        return _poetryText[tokenId];
+        return _poetryMapping[tokenId];
     }
 
     function _setPoetryText(uint256 tokenId, string memory poetryText) private {
         require(_ownerOf(tokenId) != address(0), "Token doesn't exist");
-        _poetryText[tokenId] = poetryText;
-        emit PoetryPublished(msg.sender, _tokenIdCounter, poetryText);
+        _poetryMapping[tokenId] = poetryText;
+        emit PoetryPublished(msg.sender, tokenIdCounter, poetryText);
     }
 }
